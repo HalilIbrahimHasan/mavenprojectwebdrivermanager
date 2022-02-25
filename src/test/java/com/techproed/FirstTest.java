55031aa463a37b33f3b9fca21fe5261b7cc022c2
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2022-02-25 16:48:27 -0500

    Last commit

--- src/test/java/com/techproed/FirstTest.java
+++ src/test/java/com/techproed/FirstTest.java
@@ -11,10 +11,49 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
+import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+
+
 
 public class FirstTest {
 
 
+        
+        
+     @Test
+    public void get01(){
+
+        //1.set the url
+        String url = "https://restful-booker.herokuapp.com/booking/3";
+
+        //2.Set the expected data
+
+
+        //3.Send the request and get the response
+        Response response  = given().when().get(url);
+
+        response.prettyPrint();
+
+
+
+        //Do assertions / Do validations
+        //we are making "Hard Assertion"
+        response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+
+
+
+
+
+    }
+
+        
+        
+        
+        
+        
+        
 
         @Test
         public void OpenBrowser()  {
